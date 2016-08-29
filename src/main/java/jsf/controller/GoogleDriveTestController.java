04e4cdfdb2418f7f4a03d7298a4e35584298e151
wildli0422 <wildli0422@gmail.com>
2016-08-29 21:16:31 +0800

    done environment setup (maybe.....)

---
+++ src/main/java/jsf/controller/GoogleDriveTestController.java
@@ -0,0 +1,11 @@
+package jsf.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@RequestScoped
+@ManagedBean(name="gdController")
+public class GoogleDriveTestController {
+	
+
+}
